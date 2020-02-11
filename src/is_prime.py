2059a0ec354d102c03630b0ef6c0b1389285d21e
aapetsi <apetsiampiah@gmail.com>
2020-02-11 15:20:22 +0000

    complete stretch goals

--- src/is_prime.py
+++ src/is_prime.py
@@ -6,3 +6,21 @@ def is_prime(n):
 
 
 print(is_prime(8))
+
+
+def sieve_of_erasthones(n):
+    nums = [x for x in range(2, n+1)]
+    primes = []
+    x = 2
+    while (x < n):
+        for idx, i in enumerate(nums):
+            if i % x == 0 and i >= x**2:
+                nums[idx] = False
+        x += 1
+    
+    for num in nums:
+        if type(num) == int:
+            primes.append(num)
+    return primes
+
+print(sieve_of_erasthones(50))
